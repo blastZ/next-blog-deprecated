274c457ba4623865639bb4f2d2b7b869bffb3325
daoge <daoge@dtstack.com>
2019-04-23 01:14:07 +0800

    Update

--- data/posts.js
+++ data/posts.js
@@ -1,1 +1,4 @@
-export default [require('./three-ways-to-remove-array-duplicates.json')];
+export default [
+  require('../pages/three-ways-to-remove-array-duplicates.mdx').frontMatter,
+  require('../pages/css-selector-pseudo-element-and-class.mdx').frontMatter
+];
