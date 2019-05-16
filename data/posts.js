ff44347dd1f3603a47c57d1c3571dbb0d70e27cc
daoge <daoge@dtstack.com>
2019-05-17 00:53:16 +0800

    Edit ecmascript-standard

--- data/posts.js
+++ data/posts.js
@@ -1,5 +1,6 @@
 export default [
   require('../pages/three-ways-to-remove-array-duplicates.mdx').frontMatter,
   require('../pages/css-selector-pseudo-element-and-class.mdx').frontMatter,
-  require('../pages/svg-draw-arc-with-path.mdx').frontMatter
+  require('../pages/svg-draw-arc-with-path.mdx').frontMatter,
+  require('../pages/ecmascript-standard.mdx').frontMatter
 ];
