40bd5b6f278553a98e1e4d60ddcb294ea254bc9e
daoge <daoge@dtstack.com>
2019-05-17 01:12:25 +0800

    Fix merge error

--- data/posts.js
+++ data/posts.js
@@ -1,6 +1,6 @@
 export default [
-  require('../pages/three-ways-to-remove-array-duplicates.mdx').frontMatter,
-  require('../pages/css-selector-pseudo-element-and-class.mdx').frontMatter,
-  require('../pages/svg-draw-arc-with-path.mdx').frontMatter,
-  require('../pages/ecmascript-standard.mdx').frontMatter
+  require('../pages/posts/three-ways-to-remove-array-duplicates.mdx').frontMatter,
+  require('../pages/posts/css-selector-pseudo-element-and-class.mdx').frontMatter,
+  require('../pages/posts/svg-draw-arc-with-path.mdx').frontMatter,
+  require('../pages/posts/ecmascript-standard.mdx').frontMatter
 ];
