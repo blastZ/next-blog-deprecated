30ecf62a5489db520bff2fdc29660db463b50b89
blastz <touchzh1996@gmail.com>
2019-05-23 11:01:07 +0800

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:blastZ/next-blog.git',
       path: '/home/blog',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+      'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
     }
   }
 };
