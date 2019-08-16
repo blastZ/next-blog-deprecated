c128e2dae2cb4e61ab2b30bfee7ac56e0a8862ff
blastz <touchzh1996@gmail.com>
2019-08-16 20:20:34 +0800

    Update

--- next.config.js
+++ next.config.js
@@ -2,12 +2,11 @@ const withPlugins = require('next-compose-plugins');
 const withMDX = require('@zeit/next-mdx')({
   extension: /\.mdx?$/
 });
-const withFonts = require('next-fonts');
+
 const withCSS = require('@zeit/next-css');
 
 module.exports = withPlugins([
   withCSS,
-  withFonts,
   [
     withMDX,
     {
