0de96cd6a8be5313b93d26b6b3c52193d6c3e1ce
blastz <touchzh1996@gmail.com>
2019-09-15 22:45:24 +0800

    feat(category): update category component

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,30 +1,57 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Drawer from '@material-ui/core/Drawer';
 
-const Category = ({ classes, categoryList, changeCategory }) => {
+const categories = [
+  {
+    name: 'all',
+    num: 3
+  },
+  {
+    name: 'tcp/ip',
+    num: 2
+  },
+  {
+    name: 'linux',
+    num: 2
+  },
+  {
+    name: 'javascript',
+    num: 1
+  }
+];
+
+export default ({ open, onClose, changeCategory }) => {
+  const classes = useStyles();
+
   return (
-    <List component="nav">
-      {categoryList.map((o, index) => (
-        <ListItem onClick={() => changeCategory(o.name)} key={o.name} button>
-          <ListItemText
-            classes={{
-              primary: classes.listItemText
-            }}
-            primary={`${o.name} (${o.num})`}
-          />
-        </ListItem>
-      ))}
-    </List>
+    <Drawer
+      classes={{
+        paper: classes.paper
+      }}
+      anchor="right"
+      open={open}
+      onClose={onClose}>
+      <List>
+        {categories.map((category, index) => (
+          <ListItem onClick={changeCategory(category.name)} button key={category.name}>
+            <ListItemText className={classes.listItemText} primary={`${category.name}(${category.num})`} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
 };
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   listItemText: {
-    color: '#009688'
+    color: '#009688',
+    textAlign: 'center'
+  },
+  paper: {
+    minWidth: 200
   }
-});
-
-export default withStyles(styles)(Category);
+}));
