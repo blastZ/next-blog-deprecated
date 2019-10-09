f1e138f9f09b4563675a5b721f63ddb22607686f
daoge <daoge@dtstack.com>
2019-10-10 00:00:20 +0800

    feat(blog): update posts

--- data/posts.js
+++ data/posts.js
@@ -5,5 +5,6 @@ export default [
   require('../pages/posts/three-ways-to-remove-array-duplicates.mdx').frontMatter,
   require('../pages/posts/css-selector-pseudo-element-and-class.mdx').frontMatter,
   require('../pages/posts/svg-draw-arc-with-path.mdx').frontMatter,
-  require('../pages/posts/ecmascript-standard.mdx').frontMatter
+  require('../pages/posts/ecmascript-standard.mdx').frontMatter,
+  require('../pages/posts/linux-basic-file-permission.mdx').frontMatter
 ];
